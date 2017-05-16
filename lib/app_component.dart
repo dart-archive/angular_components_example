ab75c035150096c235070849595bf8ca5caaadc7
Matan Lurey <matan@lurey.org>
2017-05-15 17:51:58 -0700

    Replace platform_* options.

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,15 +1,20 @@
 // Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-import 'package:angular2/core.dart';
+import 'package:angular2/angular2.dart';
 import 'package:angular_components/angular_components.dart';
 
 @Component(
-    selector: 'my-app',
-    templateUrl: 'app_component.html',
-    styleUrls: const ['app_component.css'],
-    directives: const [materialDirectives, defaultPopupSizeProvider],
-    providers: const [materialProviders])
+  selector: 'my-app',
+  templateUrl: 'app_component.html',
+  styleUrls: const ['app_component.css'],
+  directives: const [
+    CORE_DIRECTIVES,
+    materialDirectives,
+    defaultPopupSizeProvider,
+  ],
+  providers: const [materialProviders],
+)
 class AppComponent {
   int count = 0;
 
