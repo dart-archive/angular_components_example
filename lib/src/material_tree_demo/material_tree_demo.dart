2da0c70cfd5efb49b40c282c4e2b4d2170dfda56
Nicholas Shahan <nshahan@google.com>
2017-07-18 16:55:15 -0700

    Update angular dependency to 4.0.0-alpha
    * Fix Material List example.
    * Update SDK dependencies.
    * Remove SDK 1.23 from travis matrix.

--- lib/src/material_tree_demo/material_tree_demo.dart
+++ lib/src/material_tree_demo/material_tree_demo.dart
@@ -1,5 +1,5 @@
 import 'package:angular_components/angular_components.dart';
-import 'package:angular2/angular2.dart';
+import 'package:angular/angular.dart';
 
 /// An example that renders a [MaterialTreeComponent] with nested options.
 ///
