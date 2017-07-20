1fc2ec58b80391c68c3ec1b28319c62c4ad83d35
Nicholas Shahan <nshahan@google.com>
2017-07-20 10:37:01 -0700

    Update angular dependency to 4.0.0-alpha (#30)
    * Fix Material List example.
    * Update SDK dependencies.
    * Remove SDK 1.23 from travis matrix.

--- web/main.dart
+++ web/main.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:angular2/platform/browser.dart';
+import 'package:angular/angular.dart';
 import 'package:angular_components_example/app_component.dart';
 
 main() {
