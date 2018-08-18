000f81c49736219bac6b51067006a4515f86679d
Nicholas Shahan <nshahan@google.com>
2018-08-17 17:32:00 -0700

    Add missing demo for Material List (#98)

--- example/material_list_example/lib/examples.dart
+++ example/material_list_example/lib/examples.dart
@@ -14,6 +14,7 @@ import 'material_list_demo.dart';
     MaterialListComponent,
     MaterialListItemComponent,
   ],
+  demos: [MaterialListDemoComponent],
   benchmarks: ['material_list_item_init', 'material_list_init'],
   showGeneratedDocs: true,
 )
